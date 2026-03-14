d4490a66c74f34b138ce2f7fbdf9d21c2b93b9c9
Egor-Gerasimchik <61649237+Egor-Gerasimchik@users.noreply.github.com>
2026-03-15 04:54:35 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -87,3 +87,14 @@
         window.onload = function() {
             document.getElementById('searchInput').focus();
         };
+
+    document.addEventListener('DOMContentLoaded', () => {
+        const navToggle = document.getElementById('navToggle');
+        const navLinks = document.getElementById('navLinks');
+
+    if (navToggle && navLinks) {
+        navToggle.addEventListener('click', () => {
+        navLinks.classList.toggle('open');
+        });
+    }
+    });
